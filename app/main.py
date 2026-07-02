ae19d19e859cee31d1ffe1dd239f4ae4319da5ea
Articlar <michael.anugraha@gmail.com>
2026-07-01 20:08:01 -0700

    very early stages on hash checkings, added simple known hashes for testing

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-print("Threat Intelligence Platform")
+import hash_utils
+import json
+
+def main():
+    with open("known_hashes.json", "r") as file:
+        database = json.load(file)
+    print("Hello Threat and Hash checks: ")
+    ins = input("Input hash here:\n")
+
+    if ins in database:
+        data = database[ins]
+        print("Found in local database")
+        print("NAME: ", data["name"])
+        print("Status:", data["type"])
+        print("Description:", data["description"])
+    else:
+        print("Hash not found in local database")
+    
+    print("\nHash Analysis:")
+    print("Input: ", ins) 
+    print("Type: ", hash_utils.hash_type(ins))
+    return 
+
+if __name__ == "__main__":
+    main()
